a10c217167ac65a07c7d093c474d0d2ccac68e73
Kuesung Park <gueit214@naver.com>
2023-09-28 15:25:47 +0900

    feat : 컴퓨터 숫자값 선택 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computerNumberList = this.getRandomNumberList();
+  }
+  // 1. 컴퓨터 숫자값 선택하기
+  getRandomNumberList() {
+    const computerNumberList = [];
+    while (computerNumberList.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 10);
+      if (computerNumberList.includes(randomNumber)) continue;
+      computerNumberList.push(randomNumber);
+    }
+    return computerNumberList;
+  }
+
 }
 
 module.exports = App;
