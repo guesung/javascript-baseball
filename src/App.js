eedaf2cf038c44d62327fa12d934430fda509ea5
Kuesung Park <gueit214@naver.com>
2023-09-28 15:30:50 +0900

    feat : 게임 시작 로직 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    const computerNumberList = this.getRandomNumberList();
+    while (1) {
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+      const computerNumberList = this.getRandomNumberList();
+      const isReplay = this.doPlay(computerNumberList);
+      if (!isReplay) break;
+    }
   }
+
   // 1. 컴퓨터 숫자값 선택하기
   getRandomNumberList() {
     const computerNumberList = [];
@@ -13,6 +19,28 @@ class App {
       computerNumberList.push(randomNumber);
     }
     return computerNumberList;
+  }
+
+  // 2. 게임 시작
+  doPlay(computerNumberList) {
+    let isReplay;
+    while (isReplay === undefined) {
+      const userNumberList = this.getUserNumberList();
+      const { strikeCount, ballCount } = this.matchComputerVSUser(computerNumberList, userNumberList);
+      MissionUtils.Console.print(`${ballCount ? ballCount + '볼' : ''} ${strikeCount ? strikeCount + '스트라이크' : ''}`);
+      if (strikeCount === 3) {
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+        MissionUtils.Console.readLine("", (input) => {
+          if (input === '1') isReplay = true;
+          else if (input === '2') isReplay = false
+          else throw new Error("잘못된 값을 입력하였습니다.")
+        })
+      }
+      if (strikeCount === 0 && ballCount === 0) MissionUtils.Console.print('낫싱');
+
+    }
+    return isReplay
   }
 
 }
