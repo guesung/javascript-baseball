34f0d6bd8fcd34face0aac80e3be6598d79247b1
Kuesung Park <gueit214@naver.com>
2023-09-30 11:36:45 +0900

    style : prettier 적용

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,11 +1,11 @@
-const Message = {
-  GAME_START: "숫자 야구 게임을 시작합니다.",
-  INPUT_YOUR_GONG: "숫자를 입력해주세요 : ",
-  GUESS_GONG_RESULT_SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  REPLAY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  WRONG_INPUT: "잘못된 값을 입력하였습니다.",
-  GUESS_GONG_RESULT_STRIKE: "스트라이크",
-  GUESS_GONG_RESULT_BALL: "볼",
-  GUESS_GONG_RESULT_NOTHING: "낫싱",
+const Messages = {
+  GAME_START: '숫자 야구 게임을 시작합니다.',
+  INPUT_YOUR_GONG: '숫자를 입력해주세요 : ',
+  GUESS_GONG_RESULT_SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  REPLAY: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  WRONG_INPUT: '잘못된 값을 입력하였습니다.',
+  GUESS_GONG_RESULT_STRIKE: '스트라이크',
+  GUESS_GONG_RESULT_BALL: '볼',
+  GUESS_GONG_RESULT_NOTHING: '낫싱',
 };
-module.exports = Message;
+module.exports = Messages;
