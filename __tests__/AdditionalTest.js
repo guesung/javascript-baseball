16b1ae8e29f2857ebc00cab348431ca4e4550258
Kuesung Park <gueit214@naver.com>
2023-09-30 12:17:32 +0900

    refactor : 기존 테스트 파일 보존 & 추가적인 테스트 코드 작성

--- __tests__/AdditionalTest.js
+++ __tests__/AdditionalTest.js
@@ -20,17 +20,11 @@ const getLogSpy = () => {
 };
 
 describe('숫자 야구 게임', () => {
-  test('게임 종료 후 재시작', () => {
-    const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ['246', '135', '1', '597', '589', '2'];
+  test('1스트라이크', () => {
+    const randoms = [1, 3, 5];
+    const answers = ['126', '135', '2'];
     const logSpy = getLogSpy();
-    const messages = [
-      '낫싱',
-      '3스트라이크',
-      '1볼 1스트라이크',
-      '3스트라이크',
-      '게임 종료',
-    ];
+    const messages = ['1스트라이크', '3스트라이크', '게임 종료'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -43,16 +37,20 @@ describe('숫자 야구 게임', () => {
     });
   });
 
-  test('예외 테스트', () => {
-    const randoms = [1, 3, 5];
-    const answers = ['1234'];
+  test('낫싱', () => {
+    const randoms = [2, 5, 6];
+    const answers = ['379', '834', '147', '970', '256', '2'];
+    const logSpy = getLogSpy();
+    const messages = [...Array(5).fill('낫싱'), '게임 종료'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
 
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow();
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
 });
